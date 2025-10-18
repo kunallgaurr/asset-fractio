df15d5dc8f8755d603e2d75fe61ea9bb5821a2ba
kunallgaurr <http.kunalgaur@outlook.com>
2025-10-18 12:33:01 +0530

    Refactor AppModule to enhance middleware and module integration
    - Updated AppModule to implement NestModule interface for middleware configuration.
    - Added LoggerMiddleware and RateLimiter middleware for improved request handling.
    - Integrated GlobalModule, UserModule, CommunicationModule, and EventEmitterModule for better modularity.
    - Configured JwtModule for global usage with specified secret and expiration settings.

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,16 +1,37 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ConnectionModule } from 'src/connnection';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ConnectionModule } from '../connection';
+import { UserModule } from './user';
+import { CommunicationModule } from './communication';
+import { JwtModule } from '@nestjs/jwt';
+import { GlobalModule } from 'src/global/global.module';
+import { LoggerMiddleware } from 'src/helpers/middleware/logger.middleware';
+import { RateLimiter } from 'src/helpers/middleware/rate-limiter.middleware';
 
 @Module({
   imports: [
     ConnectionModule, 
+    GlobalModule,
+    UserModule,
+    CommunicationModule,
+    EventEmitterModule.forRoot({global: true}),
     ConfigModule.forRoot({
       isGlobal: true, 
       envFilePath: './.env'
+    }),
+    JwtModule.register({
+      global: true,
+      secret: 'your-super-secret-jwt-key-change-in-production',
+      signOptions: { expiresIn: '5d' }
     })
   ],
   controllers: [],
-  providers: [],
+  providers: [RateLimiter],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(RateLimiter).forRoutes('*');
+  }
+}
