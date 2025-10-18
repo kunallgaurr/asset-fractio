d9f98cd46b0d9503ac01d5a1270a37283b6a3d09
kunallgaurr <http.kunalgaur@outlook.com>
2025-10-18 12:32:46 +0530

    Enhance ZodValidationPipe error handling
    - Updated ZodValidationPipe to throw an HttpException with a structured response for validation errors.
    - Included HttpStatusCode and HttpStatus imports for better error management.

--- src/helpers/pipes/validation.pipe.ts
+++ src/helpers/pipes/validation.pipe.ts
@@ -1,4 +1,5 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata, HttpException, HttpCode, HttpStatus } from '@nestjs/common';
+import { HttpStatusCode } from 'axios';
 import { HttpResponse } from 'src/utils';
 import z from 'zod';
 
@@ -12,7 +13,11 @@ export class ZodValidationPipe implements PipeTransform {
             return parsedValue;
         } catch (error) {
             if (error instanceof z.ZodError) {
-                return HttpResponse.badRequest('There was somethis wrong in the payoad.', error.issues)
+                throw new HttpException({
+                    code: HttpStatusCode.BadRequest,
+                    message: 'There was somethis wrong in the payload.',
+                    result: error.issues,
+                }, HttpStatus.OK)
             }
 
             return HttpResponse.internalServerError();
