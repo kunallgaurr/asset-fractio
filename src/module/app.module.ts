3bcd28cfd47b622327b6959621f393aefbc18628
kunallgaurr <http.kunalgaur@outlook.com>
2025-10-20 02:59:29 +0530

    Refactor AppModule to include Settings and Preferences Module and Authentication Middleware
    - Added SettingsAndPreferencesModule to the AppModule imports for enhanced user settings management.
    - Integrated AuthenticationMiddleware with exclusions for signup and signin routes to improve security.
    - Maintained existing middleware configurations for Logger and Rate Limiter, ensuring consistent request handling.

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -8,30 +8,38 @@ import { JwtModule } from '@nestjs/jwt';
 import { GlobalModule } from 'src/global/global.module';
 import { LoggerMiddleware } from 'src/helpers/middleware/logger.middleware';
 import { RateLimiter } from 'src/helpers/middleware/rate-limiter.middleware';
+import { SettingsAndPreferencesModule } from './settings-and-preferences';
+import { AuthenticationMiddleware } from 'src/helpers/middleware';
 
 @Module({
   imports: [
-    ConnectionModule, 
+    ConnectionModule,
     GlobalModule,
-    UserModule,
-    CommunicationModule,
-    EventEmitterModule.forRoot({global: true}),
+    EventEmitterModule.forRoot({ global: true }),
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
       envFilePath: './.env'
     }),
     JwtModule.register({
       global: true,
       secret: 'your-super-secret-jwt-key-change-in-production',
       signOptions: { expiresIn: '5d' }
-    })
+    }),
+    UserModule,
+    CommunicationModule,
+    SettingsAndPreferencesModule
   ],
   controllers: [],
   providers: [RateLimiter],
 })
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
     consumer.apply(RateLimiter).forRoutes('*');
+    consumer.apply(AuthenticationMiddleware)
+      .exclude(
+        '/user/signup',
+        '/user/signin'
+      ).forRoutes('*');
   }
 }
